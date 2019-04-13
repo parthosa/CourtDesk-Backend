f8c37d1b084c676560c4457ac40e1926953e0164
Devansh Ghatak <devansh@avaamo.com>
2019-04-13 10:09:44 +0530

    Added URLs

--- app/urls.py
+++ app/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('get_casefile/', views.get_casefile, name='get_casefile'),
     path('get_file_stream/', views.get_file_stream, name='get_file_stream'),
     path('update_peshi/', views.update_peshi, name='update_peshi'),
-    path('update_order/', views.update_order, name='update_order')
+    path('update_order/', views.update_order, name='update_order'),
+    path('update_is_urgent/', views.update_is_urgent, name='update_is_urgent'),
+    path('update_casefile_details/', views.update_casefile_details, name='update_casefile_details'),
+    path('update_notes/', views.update_notes, name='update_notes'),
+    path('update_order_status/', views.update_order_status, name='update_order_status')
     ]
